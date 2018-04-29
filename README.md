334cd0fa06225a7dc65ed06358bbd4b55767ad83
Iconoclast <cxd4@uniplex.cat-vs-internet.gov>
2018-04-29 02:31:41 -0400

    uploaded original working source

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # wget-loop
 Simple program to automate number-range file downloads on GNU systems.
+
+## Syntax
+```sh
+wgetloop [prefix] [suffix] [count] [start]
+```
+
+Only the last parameter, `start`, is optional.  In the absence of its specification, the starting file number will be 0.
+
+## Examples
+```sh
+# Download all files from image-0.jpg to image-999.jpg.
+wgetloop http://www.example.com/image- .jpg 1000 0
+wgetloop http://www.example.com/image- .jpg 1000 # last param optional
+
+# Download all files from image-000.jpg to image-999.jpg.  (Note the leading zeroes.)
+wgetloop http://www.example.com/image- .jpg 1000 000 # not "0", "000"
+
+# Download file.jpg from domains image-000.com to image-999.com.
+wgetloop http://www.image- .com/file.jpg 1000 000
+```
---
+++ wgetloop.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <string.h>
+
+int
+main(int argc, char* argv[])
+{
+    size_t file_digits;
+    char* full_command;
+    unsigned long i, file_number, files_to_download;
+
+    if (argc < 2) {
+        fprintf(stderr, "%s:  missing base URI\n", argv[0]);
+        return 1;
+    }
+    if (argc < 3) {
+        fputs("missing common file extension or naming suffix\n", stderr);
+        return 1;
+    }
+
+    if (argc < 4) {
+        fputs("unspecified number of download iterations to run\n", stderr);
+        return 1;
+    }
+    files_to_download = strtoul(argv[3], NULL, 0);
+
+    if (argc < 5) {
+        file_number = 0;
+    } else {
+        file_number = strtoul(argv[4], NULL, 10);
+        file_digits = strlen(argv[4]);
+    }
+
+    full_command = (char *)malloc(
+        sizeof("wget")
+      + strlen(argv[1]) /* example:  "http://www.example.com/image-number-" */
+      + strlen(argv[2]) /* example:  ".jpg" */
+      + file_digits /* example:  "000000" */
+      + 1 /* in case URL counts like (9.jpg, 10.jpg), not (09.jpg, 10.jpg) */
+      + sizeof("--auth-no-challenge")
+    );
+    if (full_command == NULL) {
+        fputs("insufficient free memory available\n", stderr);
+        return 1;
+    }
+
+    for (i = 0; i < files_to_download; i++) {
+        sprintf(
+            full_command, "wget %s%.*lu%s %s",
+            argv[1], file_digits, file_number + i, argv[2],
+            "--auth-no-challenge"
+        );
+        system(full_command);
+    }
+    free(full_command);
+    return 0;
+}
